2b6e49610d9aeef7c3d64e2d8c2a1803531b184c
ENTRYFI <entryfi.entryfi@gmail.com>
2024-08-05 16:10:55 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import cv2
+
 import face_recognition as fr
 
-dar = cv2.imread('Images\darshani.jpg')
+from Images import cam
+
+from datetime import datetime 
+
+
+
+current_time = datetime.now().strftime("%Y-%m-%d-%S")
+
+dar = cv2.imread('Images\\2024-08-05-33.jpg')
 rgb_img_dar = cv2.cvtColor(dar,cv2.COLOR_BGR2RGB)
 dar_en=fr.face_encodings(rgb_img_dar)[0]
 
 
-su = cv2.imread('Images\sumu.jpg')
+su = cv2.imread(cam.cap())
 rgb_img_su = cv2.cvtColor(su,cv2.COLOR_BGR2RGB)
 su_en=fr.face_encodings(rgb_img_su)[0]
 
